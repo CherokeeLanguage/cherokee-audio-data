2827bdcf7ee6ab173cdb1961c8cacfb38c9c40a1
Michael Conrad <m.conrad.202@gmail.com>
2021-10-08 16:07:37 -0400

    misc

---
+++ create_tts_files_all.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import sys
+
+
+def main():
+    if sys.argv[0].strip() != "":
+        os.chdir(os.path.dirname(sys.argv[0]))
+
+    exec_list: list[str] = []
+    for (dirpath, dirnames, filenames) in os.walk("."):
+        for filename in filenames:
+            if filename == 'create_tts_files.py':
+                exec_list.append(os.path.join(dirpath, filename))
+
+    exec_list.sort()
+    for exec_filename in exec_list:
+        print()
+        print(f"=== {exec_filename}")
+        subprocess.run(["python", exec_filename])
+
+
+if __name__ == "__main__":
+    main()
