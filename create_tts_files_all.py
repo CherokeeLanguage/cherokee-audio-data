0e9812addb1993a139f9daf9f33da3e3d20df22d
Michael Conrad <m.conrad.202@gmail.com>
2021-10-17 11:22:55 -0400

    update

--- create_tts_files_all.py
+++ create_tts_files_all.py
@@ -1,25 +1,33 @@
+import glob
 import os
 import subprocess
 import sys
 
 
 def main():
-    if sys.argv[0].strip():
-        dir_name: str = os.path.dirname(sys.argv[0])
-        if dir_name:
-            os.chdir(dir_name)
+    argv0: str = sys.argv[0]
+    if argv0:
+        workdir: str = os.path.dirname(argv0)
+        if workdir:
+            os.chdir(workdir)
 
     exec_list: list[str] = []
-    for (dirpath, dirnames, filenames) in os.walk("."):
-        for filename in filenames:
-            if filename == 'create_tts_files.py':
-                exec_list.append(os.path.join(dirpath, filename))
-
+    exec_list.extend(glob.glob("*/create_tts_files_all.py"))
     exec_list.sort()
     for exec_filename in exec_list:
         print()
         print(f"=== {exec_filename}")
-        cp: subprocess.CompletedProcess = subprocess.run(["python", exec_filename])
+
+        script: str = f"""
+                    PS1='$'
+                    . ~/.bashrc
+                    conda deactivate
+                    conda activate CherokeeTrainingData
+                    python "{exec_filename}"            
+                    exit 0
+                    """
+
+        cp: subprocess.CompletedProcess = subprocess.run(script, shell=True, executable="/bin/bash", check=True)
         if cp.returncode > 0:
             raise Exception("Subprocess exited with ERROR")
 
