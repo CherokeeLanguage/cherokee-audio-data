2a6c38ce31e631d3fe6ac0f777b001396004fece
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-04-05 15:12:23 -0400

    ims toucan

--- create_tts_files_all.py
+++ create_tts_files_all.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+"""true" '''\'
+set -e
+eval "$(conda shell.bash hook)"
+conda deactivate
+conda activate CherokeeTrainingData
+exec python "$0" "$@"
+exit $?
+''"""
+
 import glob
 import os
 import subprocess
@@ -6,10 +16,12 @@ import sys
 
 def main():
     argv0: str = sys.argv[0]
+    workdir: str
     if argv0:
         workdir: str = os.path.dirname(argv0)
         if workdir:
             os.chdir(workdir)
+    workdir = os.getcwd()
 
     exec_list: list[str] = []
     exec_list.extend(glob.glob("*/create_tts_files.py"))
@@ -30,6 +42,25 @@ def main():
         cp: subprocess.CompletedProcess = subprocess.run(script, shell=True, executable="/bin/bash", check=True)
         if cp.returncode > 0:
             raise Exception("Subprocess exited with ERROR")
+
+    # create simple text file for use by IMS-Toucan, only selected data sets
+    ims_toucan_file = "ims-toucan.txt"
+    with open(ims_toucan_file, "w") as w:
+        for folder in ["cno", "durbin-feeling-tones", "see-say-write", "walc-1", "wwacc"]:
+            all_txt = os.path.join(folder, "all.txt")
+            if not os.path.exists(all_txt):
+                continue
+            with open(all_txt) as r:
+                for line in r:
+                    line = line.strip()
+                    parts = line.split("|")
+                    lang = parts[2]
+                    if lang != "chr":
+                        continue
+                    wav = parts[3]
+                    text = parts[6]
+                    wav = os.path.join(workdir, folder, wav)
+                    w.write(f"{wav}|{text}\n")
 
 
 if __name__ == "__main__":
