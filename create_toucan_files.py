74cb12ddbac120b92788899e46f5540772f09748
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-04-28 15:36:59 -0400

    switch back to using all available data

--- create_toucan_files.py
+++ create_toucan_files.py
@@ -13,8 +13,7 @@ import shutil
 
 def main():
     ims_toucan_basename = "ims-toucan"
-    # data_sets: list[str] = ["cno", "durbin-feeling-tones", "michael-conrad", "see-say-write", "walc-1", "wwacc"]
-    data_sets: list[str] = ["durbin-feeling-tones", "michael-conrad", "michael-conrad2", "walc-1"]
+    data_sets: list[str] = ["cno", "durbin-feeling-tones", "michael-conrad", "michael-conrad2", "see-say-write", "walc-1", "wwacc"]
     # get list of languages
     langs: set[str] = set()
     voices: set[str] = set()
